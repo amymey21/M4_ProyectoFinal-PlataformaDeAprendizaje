2569dcdfa07b2dbf02faf281aa6202e77954069a
Ayelén Silva <ayelen.sl@gmail.com>
2026-01-29 09:57:45 -0300

    Lección 1: estructura inicial con index.html, carpetas y práctica con console.log

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+// Lección 1 - Primer script de JavaScript
+// Mensaje 1
+console.log("¡Hola! Este es mi primer script de la lección 1.");
+// Mensaje 2
+console.log("Bienvenido a la aplicación de JavaScript que he creado");
+// Mensaje 3
+console.log("¡Puedes probar sus funciones!");
