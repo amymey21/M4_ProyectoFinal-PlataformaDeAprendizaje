b4fc3aa933b0b6e6994a27618bbcad9be39209bc
Ayelén Silva <ayelen.sl@gmail.com>
2026-02-19 10:24:23 -0300

    Lección 2: Calculadora básica con condicionales y validación

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,47 @@
 // Lección 1 - Primer script de JavaScript
 
 // Mensajes de consola
-console.log("¡Hola! Este es mi primer script de la lección 1.");
-console.log("Bienvenido/a la aplicación de JavaScript que he creado");
+console.log("Esta es la calculadora más básica del mundo.");
 console.log("¡Puedes probar sus funciones!");
 
 // Interacción con el usuario
 let nombre = prompt("¿Cuál es tu nombre?");
 alert("¡" + nombre + ", Me alegra tenerte por aquí!");
+
+// Lección 2 - Variables, expresiones y sentencias condicionales
+
+// Función de operaciones
+document
+  .getElementById("initBtn")
+  .addEventListener("click", function calcular() {
+    // Variables
+    const num1 = parseFloat(prompt("Ingresa un número:"));
+    const op = prompt("Ingresa la el símbolo de la operación:(+, -, *, /)");
+    const num2 = parseFloat(prompt("Ingresa otro número:"));
+
+    let resultado;
+    switch (op) {
+      case "+":
+        resultado = "La suma es: " + (num1 + num2);
+        break;
+      case "-":
+        resultado = "La resta es: " + (num1 - num2);
+        break;
+      case "*":
+        resultado = "La multiplicación es: " + num1 * num2;
+        break;
+      case "/":
+        if (num2 === 0) {
+          alert("Error:No puedes dividir por cero.");
+        } else {
+          resultado = "La división es: " + num1 / num2;
+        }
+        break;
+      default:
+        alert("Operación no válida. Por favor, ingresa +, -, *, o /.");
+    }
+    if (resultado !== undefined) {
+      console.log(resultado);
+      alert(resultado);
+    }
+  });
